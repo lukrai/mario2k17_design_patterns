90c61c6c3c45348f0029e2ab136311a7ab536c27
Lukas Raila <lukrai007@gmail.com>
2017-10-21 19:00:51 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mario2k17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
